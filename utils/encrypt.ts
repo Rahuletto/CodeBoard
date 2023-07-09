e32d43249ae6de5fec2533fcbae13695ca5fc2ba
Rahul <rahulmarban@gmail.com>
2023-07-09 18:48:56 +0530

    new encryption algo, ratelimits

--- utils/encrypt.ts
+++ utils/encrypt.ts
@@ -1,6 +1,5 @@
 // Encrypt-Decrypt
 import CryptoJS from 'crypto-js';
-var salt = CryptoJS.lib.WordArray.random(128 / 8);
 
 // PBKDF2 is a password-based key derivation function. In many applications of
 // cryptography, user security is ultimately dependent on a password, and 
@@ -8,7 +7,7 @@ var salt = CryptoJS.lib.WordArray.random(128 / 8);
 // some processing is required.
 
 export default function PBKDF2(text) {
-    return CryptoJS.PBKDF2(text, salt, {
+    return CryptoJS.PBKDF2(text, process.env.NEXT_PUBLIC_SALT, {
         keySize: 512 / 32
     }).toString();
 }
