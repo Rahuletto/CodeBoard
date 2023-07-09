aa87c443a5407fbe5c4b85aa3f320c86f71632eb
Rahul <rahulmarban@gmail.com>
2023-07-09 21:00:56 +0530

    test

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -6,6 +6,7 @@ import PBKDF2 from '../../../utils/encrypt';
 import makeid from '../../../utils/makeid';
 
 export default NextAuth({
+  session: { strategy: 'jwt' },
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
@@ -13,25 +14,26 @@ export default NextAuth({
       checks: ['none'],
     }),
   ],
-  // pages: {
-  //   signIn: '/auth/signin',
-  //   signOut: '/',
-  //   error: '/auth/error',
-  // },
+  pages: {
+    signIn: '/auth/signin',
+    signOut: '/',
+    error: '/auth/error',
+  },
   callbacks: {
-    async jwt({ token, account }) {
-      // Persist the OAuth access_token to the token right after signin
-      if (account) {
-        token.accessToken = account.access_token
-      }
-      return token
+    async redirect({ url, baseUrl }) {
+      return baseUrl;
     },
     async session({ session, token, user }) {
-      // Send properties to the client, like an access_token from a provider.
       // @ts-ignore
-      session.accessToken = token.accessToken
-      session.user = token.user;
-      return session
+      session.accessToken = token.accessToken;
+
+      return session;
+    },
+    async jwt({ token, user, account, profile }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
     },
     async signIn({ user, account, profile, email, credentials }) {
       const ifExist = await User.findOne({ id: user.id });
