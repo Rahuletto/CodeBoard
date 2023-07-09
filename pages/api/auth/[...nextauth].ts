ff3b18d32ce5bb6f805462d721ad22056658124f
Rahul <rahulmarban@gmail.com>
2023-07-09 20:27:48 +0530

    send help aaa

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -19,9 +19,19 @@ export default NextAuth({
   //   error: '/auth/error',
   // },
   callbacks: {
-    async session({ session, token }) {
+    async jwt({ token, account }) {
+      // Persist the OAuth access_token to the token right after signin
+      if (account) {
+        token.accessToken = account.access_token
+      }
+      return token
+    },
+    async session({ session, token, user }) {
+      // Send properties to the client, like an access_token from a provider.
+      // @ts-ignore
+      session.accessToken = token.accessToken
       session.user = token.user;
-      return session;
+      return session
     },
     async signIn({ user, account, profile, email, credentials }) {
       const ifExist = await User.findOne({ id: user.id });
