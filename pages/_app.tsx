6c7cd4c6e89e47b2506d1a649e88753bdec2226d
Rahul <rahulmarban@gmail.com>
2023-07-13 11:49:27 +0530

    error boundary

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,18 +10,49 @@ import { SessionContextProvider } from '@supabase/auth-helpers-react';
 // Styles
 import '../styles/globals.css';
 import '../styles/mobile.css';
+import React from 'react';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   const [supabaseClient] = useState(() => createPagesBrowserClient());
 
   return (
-    <SessionContextProvider
-      supabaseClient={supabaseClient}
-      initialSession={pageProps.initialSession}>
-      <Component {...pageProps} />
-      <Analytics />
-    </SessionContextProvider>
+    <ErrorBoundary>
+      <SessionContextProvider
+        supabaseClient={supabaseClient}
+        initialSession={pageProps.initialSession}>
+        <Component {...pageProps} />
+        <Analytics />
+      </SessionContextProvider>
+    </ErrorBoundary>
   );
 }
 
 export default MyApp;
+
+class ErrorBoundary extends React.Component {
+  state: Readonly<{ hasError: boolean }>;
+  constructor(props: any) {
+    super(props);
+
+    this.state = { hasError: false };
+  }
+  static getDerivedStateFromError(error: any) {
+    return { hasError: true };
+  }
+  componentDidCatch(error: any, errorInfo: any) {
+    console.warn({ error, errorInfo });
+  }
+  render() {
+    if (this.state.hasError) {
+      return {
+        redirect: {
+          destination: '/500',
+          permanent: false
+        }
+      }
+    }
+
+    //@ts-ignore
+    return this.props.children;
+  }
+}
