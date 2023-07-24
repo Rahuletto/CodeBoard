b0dfa0aafde5c7ba94a8acc924ef7b8174ed911b
Rahul Marban <rahulmarban@gmail.com>
2023-07-24 11:53:49 +0000

    fix

--- middleware.ts
+++ middleware.ts
@@ -157,10 +157,6 @@ export const config = {
 function generateUUID() {
   var d = new Date().getTime();
 
-  if (window.performance && typeof window.performance.now === "function") {
-    d += performance.now();
-  }
-
   var uuid = 'codeboard_api.xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
     var r = (d + Math.random() * 16) % 16 | 0;
     d = Math.floor(d / 16);
